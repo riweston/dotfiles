00dafb6d9d61a8b9938cf372c0f10a4bc10c8165
Richard Weston <github@riweston.io>
2022-07-04 15:19:03 +0100

    Update dot_config/private_fish/config.fish Update dot_config/private_fish/fish_variables

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -2,6 +2,8 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+source /opt/homebrew/opt/asdf/libexec/asdf.fish
+
 starship init fish | source
 
 set -gx GOPATH (go env GOPATH)
